104ba1ece93dd5358d024aa76844b912333e1807
nitvech25 <63171909+nitvech25@users.noreply.github.com>
2025-09-02 20:41:02 -0400

    Update Exoplanet_Detector.py

--- Exoplanet_Detector.py
+++ Exoplanet_Detector.py
@@ -1,1 +1,172 @@
+!wget -q --show-progress 'https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20Planet%20Hunters/exoTrain.csv'
+!wget -q --show-progress 'https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20Planet%20Hunters/exoTest.csv'
 
+from urllib.request import urlretrieve
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.cluster import KMeans
+from sklearn import  metrics
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from scipy.signal import savgol_filter
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score,ConfusionMatrixDisplay,precision_score,recall_score,f1_score
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, normalize
+
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import optimizers
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import Adam, SGD
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv1D, Conv2D, MaxPooling2D, BatchNormalization, MaxPooling1D
+from keras.losses import categorical_crossentropy
+from keras.regularizers import l2
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import load_model
+
+df_train = pd.read_csv('exoTrain.csv')
+df_train['LABEL'] = df_train['LABEL'] -1
+df_test = pd.read_csv('exoTest.csv')
+df_test['LABEL'] = df_test['LABEL'] - 1
+
+def plot_graphs(history, best):
+
+  plt.figure(figsize=[10,4])
+  # summarize history for accuracy
+  plt.subplot(121)
+  plt.plot(history.history['accuracy'])
+  plt.plot(history.history['val_accuracy'])
+  plt.title('model accuracy across training\n best accuracy of %.02f'%best[1])
+  plt.ylabel('accuracy')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+
+  # summarize history for loss
+  plt.subplot(122)
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('model loss across training\n best loss of %.02f'%best[0])
+  plt.ylabel('loss')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+  plt.show()
+
+def analyze_results(model, X_train, y_train, X_test, y_test):
+    """
+    Helper function to help interpret and model performance.
+
+    Args:
+    model: estimator instance
+    X_train: {array-like, sparse matrix} of shape (n_samples, n_features)
+    Input values for model training.
+    y_train : array-like of shape (n_samples,)
+    Target values for model training.
+    X_test: {array-like, sparse matrix} of shape (n_samples, n_features)
+    Input values for model testing.
+    y_test : array-like of shape (n_samples,)
+    Target values for model testing.
+
+    Returns:
+    None
+    """
+    print("-------------------------------------------")
+    print("Model Results")
+    print("")
+    print("Training:")
+    if type(model) == keras.src.engine.sequential.Sequential:
+      train_predictions = model.predict(X_train)
+      train_predictions = (train_predictions > 0.5)
+      cm = confusion_matrix(y_train, train_predictions)
+      labels = [0, 1]
+      df_cm = pd.DataFrame(cm,index = labels,columns = labels)
+      fig = plt.figure()
+      res = sns.heatmap(df_cm, annot=True,cmap='Blues', fmt='g')
+      #plt.yticks([1.25, 3.75], labels,va='center')
+      plt.title('Confusion Matrix - TestData')
+      plt.ylabel('True label')
+      plt.xlabel('Predicted label')
+      plt.show()
+    else:
+      plt.close()
+      ConfusionMatrixDisplay.from_estimator(model,X_train,y_train)
+      plt.show()
+
+    print("Testing:")
+    if type(model) == keras.src.engine.sequential.Sequential:
+      test_predictions = model.predict(X_test)
+      test_predictions = (test_predictions > 0.5)
+      cm = confusion_matrix(y_test, test_predictions)
+      labels = [0, 1]
+      df_cm = pd.DataFrame(cm,index = labels,columns = labels)
+      fig = plt.figure()
+      res = sns.heatmap(df_cm, annot=True,cmap='Blues', fmt='g')
+      #plt.yticks([1.25, 3.75], labels,va='center')
+      plt.title('Confusion Matrix - TestData')
+      plt.ylabel('True label')
+      plt.xlabel('Predicted label')
+      plt.show()
+    else:
+      ConfusionMatrixDisplay.from_estimator(model,X_test,y_test)
+
+X_train = df_train.drop('LABEL', axis=1)
+y_train = df_train['LABEL']
+X_test = df_test.drop('LABEL', axis=1)
+y_test = df_test['LABEL']
+
+# Helper functions that we can run for the three augmentation functions that will be used
+def smote(a,b):
+    model = SMOTE()
+    X,y = model.fit_resample(a, b)
+    return X,y
+
+def savgol(df1,df2):
+    x = savgol_filter(df1,21,4,deriv=0)
+    y = savgol_filter(df2,21,4,deriv=0)
+    return x,y
+
+def fourier(df1,df2):
+    X_train = np.abs(np.fft.fft(df1, axis=1))
+    X_test = np.abs(np.fft.fft(df2, axis=1))
+    return X_train,X_test
+
+def norm(df1,df2):
+    X_train = normalize(df1)
+    X_test = normalize(df2)
+    return X_train,X_test
+
+def robust(df1,df2):
+    scaler = RobustScaler()
+    X_train = scaler.fit_transform(df1)
+    X_test = scaler.transform(df2)
+    return X_train,X_test
+
+fourier_X_train, fourier_X_test = fourier(X_train, X_test)
+savgol_X_train, savgol_X_test = savgol(fourier_X_train, fourier_X_test)
+norm_X_train, norm_X_test = norm(savgol_X_train,savgol_X_test)
+robust_X_train, robust_X_test = robust(norm_X_train, norm_X_test)
+smote_X_train,smote_y_train = smote(robust_X_train, y_train
+
+model = MLPClassifier(hidden_layer_sizes = (10), activation = 'relu', random_state = 1, max_iter = 300)
+model.fit(smote_X_train, smote_y_train)
+analyze_results(model, smote_X_train, smote_y_train, X_test, y_test)
